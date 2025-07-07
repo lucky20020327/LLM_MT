ca6d753cb0191bf5a65921a46bf2d86d0519d218
Kunyi Li <lkythu@163.com>
2025-07-07 22:08:12 +0800

    v0.9: refector: add mr_generator for future extension of other mr generation methods. feat: add support for non-local function for R and python

---
+++ dataset_construction/decision_support_system/python/get_api_info.py
@@ -0,0 +1,206 @@
+import inspect
+import types
+import json
+import os
+
+
+def get_signature(obj):
+    try:
+        return str(inspect.signature(obj))
+    except Exception:
+        return "(Signature not available)"
+
+
+def get_source(obj):
+    try:
+        return inspect.getsource(obj)
+    except Exception:
+        return "(Source not available)"
+
+
+def get_doc(obj):
+    doc = inspect.getdoc(obj)
+    return doc if doc else None
+
+
+def is_defined_in_package(obj, now_module_name):
+    module_name = getattr(obj, "__module__", "") or ""
+    # print(f"Module name: {module_name}")
+    # print(getattr(obj, "__module__", ""))
+    return module_name == now_module_name
+
+
+def function_filter(obj, name, module_name):
+    """Check if the object is a function that we want to include."""
+    if not inspect.isfunction(obj):
+        return False
+    # Exclude abstract methods and methods defined in other modules
+    if getattr(obj, "__isabstractmethod__", False):
+        return False
+    if not is_defined_in_package(obj, module_name):
+        return False
+    # Exclude functions without docstrings
+    if get_doc(obj) is None:
+        return False
+    else:
+        doc = get_doc(obj)
+        if "param" not in doc.lower() or "return" not in doc.lower():
+            return False
+    # Exclude functions that are possible wrappers or decorators
+    try:
+        source = inspect.getsource(obj)
+        if "wrapper" in source or "decorator" in source:
+            return False
+    except Exception:
+        pass
+    if name == "main":
+        return False
+
+    # If all checks passed, include the function
+    return True
+
+
+def list_package_api(module, module_name, prefix=""):
+    print(f"Listing API for {module_name} with prefix {prefix}")
+    api_info = []
+    for name, obj in inspect.getmembers(module):
+        if name.startswith("_"):
+            continue  # Skip private/internal names
+
+        # Recurse into submodules
+        if inspect.ismodule(obj) and getattr(obj, "__name__", "").startswith(
+            module_name
+        ):
+            # list_package_api(obj, prefix=f"{full_name}.")
+            pass
+
+        # Handle classes
+        elif inspect.isclass(obj) and is_defined_in_package(obj, module_name):
+            class_data = {
+                "type": "class",
+                "package": module_name,
+                "name": name,
+                "signature": get_signature(obj),
+                "docstring": get_doc(obj),
+                "source_code": get_source(obj),
+                "methods": [],
+            }
+
+            # Collect methods
+            try:
+                for method_name, method in inspect.getmembers(obj):
+                    if method_name.startswith("_"):
+                        continue
+                    if function_filter(method, method_name, module_name):
+                        method_data = {
+                            "package": module_name,
+                            "name": method_name,
+                            "signature": get_signature(method),
+                            "docstring": get_doc(method),
+                            "source_code": get_source(method),
+                        }
+                        class_data["methods"].append(method_data)
+            except Exception as e:
+                print(f"Error processing class {module_name}.{name}: {e}")
+                continue
+            if class_data["methods"] == []:
+                continue  # Skip classes without methods
+            api_info.append(class_data)
+
+        # Handle standalone functions
+        elif function_filter(obj, name, module_name):
+            func_data = {
+                "type": "function",
+                "package": module_name,
+                "name": name,
+                "signature": get_signature(obj),
+                "docstring": get_doc(obj),
+                "source_code": get_source(obj),
+                "mutations": [],
+            }
+            api_info.append(func_data)
+
+    return api_info
+
+
+def list_package_api_traverse(package_name: str):
+
+    package_obj = __import__(package_name, fromlist=[""])
+    package_dir = os.path.dirname(package_obj.__file__)
+    modules_name = [
+        name[:-3]
+        for name in os.listdir(package_dir)
+        if not name.startswith("_")
+        and name.endswith(".py")
+        and name != "__init__.py"
+        and os.path.isfile(os.path.join(package_dir, name))
+    ]
+    sub_packages = [
+        name
+        for name in os.listdir(package_dir)
+        if os.path.isdir(os.path.join(package_dir, name))
+        and not name.startswith("_")
+        and name != "__pycache__"
+        and "__init__.py" in os.listdir(os.path.join(package_dir, name))
+    ]
+    print(f"Modules inside {package_dir}: {modules_name}")
+    print(f"Sub-packages inside {package_dir}: {sub_packages}")
+
+    api_info_whole = []
+
+    api_info = list_package_api(package_obj, package_name, prefix=f"{package_name}.")
+
+    print(f"API information for {package_name} collected.")
+    if api_info != []:
+        api_info_whole.extend(api_info)
+
+    for module_name in modules_name:
+        module_obj = __import__(f"{package_name}.{module_name}", fromlist=[""])
+        api_info = list_package_api(
+            module_obj,
+            f"{package_name}.{module_name}",
+            prefix=f"{package_name}.{module_name}.",
+        )
+        print(f"API information for {package_name}.{module_name} collected.")
+        if api_info != []:
+            api_info_whole.extend(api_info)
+
+    for sub_package in sub_packages:
+        api_info_whole.extend(
+            list_package_api_traverse(f"{package_name}.{sub_package}")
+        )
+
+    return api_info_whole
+
+
+if __name__ == "__main__":
+
+    import skcriteria
+
+    package_name = "skcriteria"  # Replace with the package you want to analyze
+
+    api_info = list_package_api_traverse(package_name)
+
+    # Save the API information to a JSON file
+    with open(f"{package_name}_api.json", "w", encoding="utf-8") as f:
+        json.dump(api_info, f, indent=2, ensure_ascii=False)
+
+    # import inspect
+    # import abc
+    # from abc import ABC, abstractmethod
+
+    # def is_abstract_function(obj):
+    #     return inspect.isfunction(obj) and getattr(obj, "__isabstractmethod__", False)
+    # def is_property(obj):
+    #     return isinstance(obj, property)
+    # # Example usage
+    # class MyBase(ABC):
+    #     @property
+    #     def my_property(self):
+    #         """This is a property."""
+    #         return "property value"
+    #     @abc.abstractmethod
+    #     def my_method(self):
+    #         pass
+    # print(inspect.isfunction(MyBase.my_property))
+    # print(is_property(MyBase.__dict__['my_property']))  # True
